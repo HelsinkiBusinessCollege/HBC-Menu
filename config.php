43ed5542240ea1b744f66da4743c5ff23a7af41c
teemusillantaus <teemu.sillantaus@pp.inet.fi>
2015-02-06 13:46:15 +0200

    Lisää taulut

--- config.php
+++ config.php
@@ -7,4 +7,9 @@ $sql_pass = "";
 
 $conn = new PDO('mysql:host='.$sql_host.';dbname='.$sql_db, $sql_user, $sql_pass);
 
+
+$conn->query('CREATE TABLE IF NOT EXISTS `ruoka-arvostelut` (`arvosteluID` int(11) NOT NULL AUTO_INCREMENT,`ruokaID` int(10) NOT NULL,`arvosana` int(1) NOT NULL,`pvm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (`arvosteluID`),UNIQUE KEY `arvosteluID` (`arvosteluID`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+			  CREATE TABLE IF NOT EXISTS `ruokalajit` (`ruokaID` int(10) NOT NULL AUTO_INCREMENT,`kategoria` varchar(20) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,`ruuanNimi` varchar(200) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,`tiedot` varchar(20) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,`kieli` varchar(2) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,PRIMARY KEY (`ruokaID`),UNIQUE KEY `ruuanNimi` (`ruuanNimi`),UNIQUE KEY `ruokaID` (`ruokaID`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+			  CREATE TABLE IF NOT EXISTS `ruokalista` (`listaID` int(10) NOT NULL AUTO_INCREMENT, `paiva` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,`ruokaID` varchar(100) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,`ruokaID_en` varchar(100) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,`lisayspvm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (`listaID`),UNIQUE KEY `paiva` (`paiva`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;');
+
 ?>
